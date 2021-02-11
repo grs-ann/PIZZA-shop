0ec553efeba04b4814a9fbd6ceb2ae723d1fc068
grs-ann <66564854+grs-ann@users.noreply.github.com>
2021-02-11 13:58:41 +0400

    Added OrderController, Changed Order class(entity), a little bit changes in _Layout.cs(add button to get orders)

---
+++ PizzaShopApplication/PizzaShopApplication/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaShopApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Content("Заказы будут тут.");
+        }
+    }
+}
--- PizzaShopApplication/PizzaShopApplication/Models/Data/Entities/Data/Order.cs
+++ PizzaShopApplication/PizzaShopApplication/Models/Data/Entities/Data/Order.cs
@@ -10,29 +10,34 @@ namespace PizzaShopApplication.Models.Data
     // с полями, соответствующими определенным свойствам.
     public class Order
     {
+        // Номер заказа.
         [Key]
         public int Id { get; set; }
         // Имя заказчика.
-        [Required]
-        [MaxLength(30)]
+        [Required(ErrorMessage = "Пожалуйста, укажите ваше имя")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Имя должно быть в пределах от 3 до 30 символов")]
         public int Name { get; set; }
         // Номер телефона заказчика.
-        [Required]
-        public int Phone { get; set; }
+        [Required(ErrorMessage = "Пожалуйста, укажите ваш номер телефона")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Некорректный номер телефона")]
+        public string Phone { get; set; }
         // Почта заказчика.
-        [Required]
-        [MaxLength(30)]
+        [StringLength(50, MinimumLength = 7, ErrorMessage = "Адрес почты должен быть в пределах от 7 до 50 символов")]
         public string Email { get; set; }
         // Улица, указанная заказчиком.
-        [Required]
-        [MaxLength(30)]
+        [Required(ErrorMessage = "Пожалуйста, укажите улицу для доставки.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Длина назавания улицы от 1 до 50 символов!")]
         public string Street { get; set; }
         // Номер дома, указанный заказчиком.
-        [Required]
+        [Required(ErrorMessage = "Пожалуйста, укажите номер дома для доставки")]
+        [StringLength(5, MinimumLength = 1, ErrorMessage ="Номер дома должен быть в пределах от 1 до 5 символов!")]
         public string Home { get; set; }
         // Квартира, указанная заказчиком
-        public int Apartment { get; set; }
+        [StringLength(5, MinimumLength = 1, ErrorMessage = "Номер квартиры должен быть в пределах от 1 до 5 символов!")]
+        public string Apartment { get; set; }
         // Комментарий к заказу.
+        [MaxLength(200, ErrorMessage = "Слишком длинный комментарий. Максимум - 200 символов")]
         public string Comment { get; set; }
+
     }
 }
